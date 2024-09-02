a3da526a0c8769edf75071e1879e3372a86729ea
BudgetCOntrol <140796512+BudgetControl@users.noreply.github.com>
2024-09-02 17:50:56 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,4 @@
  $app->get('/{wsid}/payees', \Budgetcontrol\Debt\Controller\DebtController::class . ':getPayees');
  $app->delete('/{wsid}/debt/{debt_id}', \Budgetcontrol\Debt\Controller\DebtController::class . ':delete');
 
-$app->get('/monitor', 'Budgetcontrol\Core\Http\Controller\Controller:monitor');
+$app->get('/monitor', 'Budgetcontrol\Debt\Http\Controller\Controller:monitor');
