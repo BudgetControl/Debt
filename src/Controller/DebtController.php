d3c3f18c1e50be8e26d0f6b4bb671a04de7980a3
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2024-11-13 14:02:47 +0100

    added new route

--- src/Controller/DebtController.php
+++ src/Controller/DebtController.php
@@ -11,63 +11,12 @@ use Budgetcontrol\Library\Model\Payee;
 class DebtController extends Controller {
 
     /**
-     * Retrieves the payees.
-     *
-     * @param Request $request The HTTP request object.
-     * @param Response $response The HTTP response object.
-     * @param array $args The route parameters.
-     * @return Response The HTTP response.
-     */
-    public function getPayees(Request $request, Response $response, array $args): Response {
-
-        $wsid = (int) $args['wsid'];
-        $repository = new DebtRepository($wsid);
-        $payeesList = $repository->getPayeesWithEntry();
-        $creditCardsDebts = $repository->getCreditCardsDebts();
-
-        if(empty($payeesList) && empty($creditCardsDebts)) {
-            return response(['message' => 'No payees found'], 404);
-        }
-
-        $payees = $payeesList->toArray();
-        $creditCards = $creditCardsDebts->toArray();
-
-        $results = array_merge($payees, $creditCards);
-
-        return response($results);
-    }
-
-    /**
-     * Delete a debt.
-     *
-     * @param Request $request The HTTP request object.
-     * @param Response $response The HTTP response object.
-     * @param array $args The route parameters.
-     * @return Response The updated HTTP response object.
-     */
-    public function delete(Request $request, Response $response, array $args): Response {
-
-        $wsid = (int) $args['wsid'];
-        $debtId = $args['debt_id'];
-
-        $repository = new DebtRepository($wsid);
-        $payee = $repository->getPayeeByUuid($debtId);
-
-        if ($payee) {
-            $payee->delete();
-            return response(['message' => 'Payee deleted successfully']);
-        }
-
-        return response(['message' => 'Payee not found'], 404);
-    }
-
-    /**
-     * Retrieves the total debt for all credit cards.
+     * Retrieves the list of payees.
      *
      * @param Request $request The HTTP request object.
      * @param Response $response The HTTP response object.
      * @param array $args The route arguments.
-     * @return Response The HTTP response with the total credit card debt.
+     * @return Response The HTTP response with the list of payees.
      */
     public function getCreditCardsDebts(Request $request, Response $response, array $args): Response 
     {
